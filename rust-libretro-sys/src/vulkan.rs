bb086ed0953a191452f690d86f9ffd64ddc38036
fireball <fb@none.none>
2025-10-24 00:32:52 -0500

    add complete lifetimes set

--- rust-libretro-sys/src/vulkan.rs
+++ rust-libretro-sys/src/vulkan.rs
@@ -5,20 +5,21 @@
 //! but their Ash bindings drop this prefix in favor of the "vk" module
 //! (e.g. "VkDevice" becomes "vk::Device").
 
-pub use ash;
 use ash::vk::*;
 
 pub type PFN_vkGetInstanceProcAddr = Option<ash::vk::PFN_vkGetInstanceProcAddr>;
 pub type PFN_vkGetDeviceProcAddr = Option<ash::vk::PFN_vkGetDeviceProcAddr>;
-pub type VkApplicationInfo = Option<ash::vk::ApplicationInfo>; 
-pub type VkInstanceCreateInfo = Option<ash::vk::InstanceCreateInfo>;
-pub type VkDeviceCreateInfo = Option<ash::vk::DeviceCreateInfo>;
+pub type VkApplicationInfo<'a> = ApplicationInfo<'a>; 
+pub type VkInstanceCreateInfo<'a> = InstanceCreateInfo<'a>;
+pub type VkDeviceCreateInfo<'a> = DeviceCreateInfo<'a>;
+pub type VkImageViewCreateInfo<'a> = ImageViewCreateInfo<'a>; 
+
+pub type VkDevice = ash::Device;
+pub type VkInstance = ash::Instance;
+
 pub type VkCommandBuffer = CommandBuffer;
-pub type VkDevice = Device;
 pub type VkImageLayout = ImageLayout;
 pub type VkImageView = ImageView;
-pub type VkImageViewCreateInfo = Option<ash::vk::ImageViewCreateInfo>; 
-pub type VkInstance = Instance;
 pub type VkPhysicalDevice = PhysicalDevice;
 pub type VkPhysicalDeviceFeatures = PhysicalDeviceFeatures;
 pub type VkQueue = Queue;
