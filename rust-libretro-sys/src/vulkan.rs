0b090d70ac5ff3f2da391ba05167cd96e3938c49
fireball <fb@none.none>
2025-10-24 14:42:12 -0500

    try reverting back to vk types

--- rust-libretro-sys/src/vulkan.rs
+++ rust-libretro-sys/src/vulkan.rs
@@ -1,21 +1,21 @@
-//#![cfg(feature = "vulkan")]
+#![cfg(feature = "vulkan")]
 
 //! Aliases for various Vulkan types that libretro_vulkan.h uses.
 //! The original types are all prefixed with "Vk",
 //! but their Ash bindings drop this prefix in favor of the "vk" module
 //! (e.g. "VkDevice" becomes "vk::Device").
 
-/*use ash::vk::*;
+use ash::vk::*;
 
-pub type PFN_vkGetInstanceProcAddr = Option<ash::vk::PFN_vkGetInstanceProcAddr>;
-pub type PFN_vkGetDeviceProcAddr = Option<ash::vk::PFN_vkGetDeviceProcAddr>;
-pub type VkApplicationInfo<'a> = ApplicationInfo<'a>; 
+pub type PFN_vkGetInstanceProcAddr = PFN_vkGetInstanceProcAddr;
+pub type PFN_vkGetDeviceProcAddr = PFN_vkGetDeviceProcAddr;
+pub type VkApplicationInfo<'a> = ApplicationInfo<'a>;
 pub type VkInstanceCreateInfo<'a> = InstanceCreateInfo<'a>;
 pub type VkDeviceCreateInfo<'a> = DeviceCreateInfo<'a>;
-pub type VkImageViewCreateInfo<'a> = ImageViewCreateInfo<'a>; 
+pub type VkImageViewCreateInfo<'a> = ImageViewCreateInfo<'a>;
 
-pub type VkDevice = ash::Device;
-pub type VkInstance = ash::Instance;
+pub type VkDevice = Device;
+pub type VkInstance = Instance;
 
 pub type VkCommandBuffer = CommandBuffer;
 pub type VkImageLayout = ImageLayout;
@@ -24,4 +24,4 @@ pub type VkPhysicalDevice = PhysicalDevice;
 pub type VkPhysicalDeviceFeatures = PhysicalDeviceFeatures;
 pub type VkQueue = Queue;
 pub type VkSemaphore = Semaphore;
-pub type VkSurfaceKHR = SurfaceKHR;*/
+pub type VkSurfaceKHR = SurfaceKHR;
